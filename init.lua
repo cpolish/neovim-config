e501fec4ce1573d4d07e2765c7bc45e9c136f27e
cpolish <68736710+cpolish@users.noreply.github.com>
2025-01-03 21:37:21 +1100

    Create initial config setup

---
+++ init.lua
@@ -0,0 +1,15 @@
+-- Set <space> as leader key (done before plugins are loaded)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Nerd font setup
+vim.g.have_nerd_font = true
+
+-- General Neovim options
+require("options")
+
+-- Basic keymaps
+require("keymaps")
+
+-- Bootstrap/install `lazy.nvim` package manager, and onfigure and install plugins
+require("lazy-setup")
---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    version = '*',
+    event = "BufEnter",
+    -- event = "VeryLazy",
+    -- lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      options = {
+        diagnostics = "nvim_lsp",
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "File Explorer",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+      },
+    },
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+      { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+      { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+      { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+      { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+      { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+      { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+    },
+    -- config = function(_, opts)
+    --   require("bufferline").setup(opts)
+    --
+    --   -- Fix bufferline when restoring a session
+    --   vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+    --     callback = function()
+    --       vim.schedule(function()
+    --         pcall(nvim_bufferline)
+    --       end)
+    --     end,
+    --   })
+    -- end
+  }
+}
