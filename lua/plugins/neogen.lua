9cbde002ea81aff2a6d08fbaf82e849bc67f695e
cpolish <68736710+cpolish@users.noreply.github.com>
2025-01-08 20:27:37 +1100

    Add and configure Neogen for code documentation

---
+++ lua/plugins/neogen.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "danymat/neogen",
+    opts = {
+      snippet_engine = "luasnip",
+    },
+    keys = {
+      { "<leader>cd", "<cmd>Neogen<CR>", desc = "Generate [C]ode [D]ocumentation" },
+    }
+  },
+}
