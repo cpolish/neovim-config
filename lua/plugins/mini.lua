5d193922fe56072b0ddccc987151a58c7add91cf
cpolish <68736710+cpolish@users.noreply.github.com>
2025-01-15 22:14:20 +1100

    Add more lazy loading to some plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,28 +1,25 @@
+-- Collection of various small independent plugins/modules
 return {
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
+  {
+    "echasnovski/mini.ai",
+    version = '*',
+    event = "VeryLazy",
+    opts = {
+      n_lines = 500,
+    },
+  },
+  {
+    "echasnovski/mini.surround",
+    version = '*',
+    event = "VeryLazy",
+    opts = {},
+  },
+  {
+    "echasnovski/mini.statusline",
+    version = '*',
     config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      local statusline = require("mini.statusline")
+      statusline.setup({ use_icons = vim.g.have_nerd_font })
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
@@ -31,15 +28,19 @@ return {
       statusline.section_location = function()
         return '%2l:%-2v'
       end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
-
-      -- Buffer deletion fixes
-      local bufremove = require("mini.bufremove")
-      bufremove.setup()
-
-      vim.keymap.set('n', "<leader>bd", bufremove.delete, { desc = "Delete Buffer" })
     end,
+  },
+  {
+    "echasnovski/mini.bufremove",
+    version = '*',
+    keys = {
+      {
+        "<leader>bd",
+        function()
+          require("mini.bufremove").delete()
+        end,
+        desc = "Delete Buffer",
+      },
+    },
   },
 }
