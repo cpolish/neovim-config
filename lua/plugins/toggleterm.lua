1c207348fa940283f2c3596cd23f372b2138eed6
cpolish <68736710+cpolish@users.noreply.github.com>
2025-01-25 11:43:11 +1100

    Modify `toggleterm` terminal size

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,11 +3,11 @@ return {
     "akinsho/toggleterm.nvim",
     version = '*',
     opts = {
-      size = 20,
+      size = 18,
       open_mapping = "<c-`>",
     },
     keys = {
       { "<c-`>", "<cmd>ToggleTerm<CR>", desc = "Toggle Terminal" }
-    }
+    },
   },
 }
