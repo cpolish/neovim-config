7a3e8f51c0e533098139c42a3a3c38af7fc791d8
cpolish <68736710+cpolish@users.noreply.github.com>
2025-03-31 21:13:44 +1100

    Replace nvim-cmp with blink.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,8 +26,8 @@ return {
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
 
-      -- Allows extra capabilities provided by nvim-cmp
-      'hrsh7th/cmp-nvim-lsp',
+      -- Allows extra capabilities provided by blink.nvim
+      "saghen/blink.cmp",
     },
     config = function()
       -- Brief aside: **What is LSP?**
@@ -171,13 +171,6 @@ return {
         vim.diagnostic.config { signs = { text = diagnostic_signs } }
       end
 
-      -- LSP servers and clients are able to communicate to each other what features they support.
-      --  By default, Neovim doesn't support everything that is in the LSP specification.
-      --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --
@@ -250,6 +243,14 @@ return {
       mason_tool_installer.setup({ ensure_installed = ensure_installed })
       mason_tool_installer.check_install()
 
+      -- LSP servers and clients are able to communicate to each other what features they
+      -- support.
+      -- By default, Neovim doesn't support everything that is in the LSP specification.
+      -- When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
+      -- So, we create new capabilities with nvim cmp, and then broadcast that to the
+      -- servers.
+      local blink_cmp = require("blink-cmp")
+
       require('mason-lspconfig').setup({
         ensure_installed = {},
         automatic_installation = false,
@@ -259,7 +260,8 @@ return {
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+            server.capabilities = blink_cmp.get_lsp_capabilities(server.capabilities,
+                                                                 true)
             require('lspconfig')[server_name].setup(server)
           end,
         },
