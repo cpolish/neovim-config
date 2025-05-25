bcc71e9d21f8777e6134bce754d82b5562ae07ca
cpolish <68736710+cpolish@users.noreply.github.com>
2025-05-25 12:42:52 +1000

    Add keymap for explaining Rust error

--- lua/plugins/languages/rust.lua
+++ lua/plugins/languages/rust.lua
@@ -3,5 +3,18 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^6",
     lazy = false,
+    init = function()
+      vim.g.rustaceanvim = {
+        server = {
+          on_attach = function(_, bufnr)
+            vim.keymap.set('n', "<leader>cx", "<cmd>RustLsp explainError<CR>",
+                           { buffer = bufnr, desc = "LSP: E[x]plain Rust Error" })
+          end,
+        },
+        tools = {
+          float_win_config = { border = "rounded" },
+        },
+      }
+    end
   },
 }
