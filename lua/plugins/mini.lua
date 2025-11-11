5f117449d61a179b85f0c270379a9d90c99475fa
cpolish <68736710+cpolish@users.noreply.github.com>
2025-11-11 14:58:09 +1100

    Make `mini.surround` keybindings more like `vim-surround`

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -77,7 +77,38 @@ return {
     "nvim-mini/mini.surround",
     version = '*',
     event = "VeryLazy",
-    opts = {},
+    opts = {
+      mappings = {
+        add = "ys",
+        delete = "ds",
+        find = "",
+        find_left = "",
+        highlight = "",
+        replace = "cs",
+      },
+      search_method = "cover_or_next",
+    },
+    config = function()
+      require("mini.surround").setup({
+        mappings = {
+          add = "ys",
+          delete = "ds",
+          find = "",
+          find_left = "",
+          highlight = "",
+          replace = "cs",
+        },
+        search_method = "cover_or_next",
+      })
+
+      -- Extra key mappings to make it more like 'vim-surround'
+      vim.keymap.del('x', "ys")
+      vim.keymap.set('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]],
+                     { silent = true })
+
+      -- Make special mapping for "add surrounding for line"
+      vim.keymap.set('n', "yss", "ys_", { remap = true })
+    end,
   },
   {
     "nvim-mini/mini.statusline",
