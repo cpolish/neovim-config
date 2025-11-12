fcca6555844278189caab383f531aafbf934e69d
cpolish <68736710+cpolish@users.noreply.github.com>
2025-11-12 13:42:08 +1100

    Add `nvim-colorizer` plugin to add virtual text colours

---
+++ lua/plugins/doc-colors.lua
@@ -0,0 +1,41 @@
+local CSS_COLOR_OPTS = {
+  names = true,
+  RGB = true,
+  RGBA = true,
+  RRGGBB = true,
+  RRGGBBAA = true,
+  AARRGGBB = true,
+  rgb_fn = true,
+  hsl_fn = true,
+  oklch_fn = true,
+}
+
+return {
+  {
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    opts = { -- set to setup table
+      filetypes = {
+        css = CSS_COLOR_OPTS,
+        scss = CSS_COLOR_OPTS,
+        sass = CSS_COLOR_OPTS,
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "html",
+        "htmldjango",
+        "razor",
+      },
+      user_default_options = {
+        names = false,
+        RGB = false,
+        RGBA = false,
+        RRGGBB = false,
+        tailwind = "lsp",
+        mode = "virtualtext",
+        virtualtext_inline = "before",
+      },
+    },
+  },
+}
