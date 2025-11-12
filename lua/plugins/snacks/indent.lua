b8e6e8bd35ac7e42b53dbabb4d96d3b30f794da7
cpolish <68736710+cpolish@users.noreply.github.com>
2025-11-12 13:41:45 +1100

    Replace indent-blankline with Snacks indent
    Due to upstream bugs being fixed.

---
+++ lua/plugins/snacks/indent.lua
@@ -0,0 +1,14 @@
+return {
+  "snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    indent = {
+      indent = { char = '▏' },
+      animate = { enabled = false },
+      scope = {
+        char = '▏',
+        underline = true,
+      },
+    },
+  },
+}
